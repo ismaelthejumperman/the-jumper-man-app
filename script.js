474fb240464cecfcf2433f1c3f6c4fa6a5081f46
ismaelthejumperman <ismaelthejumperman@gmail.com>
2025-04-06 16:28:42 -0700

    Add files via upload

--- script.js
+++ script.js
@@ -1,26 +1,8 @@
-document.getElementById("contact-form").addEventListener("submit", function(event) {
-    event.preventDefault();
-    
-    // Simple form validation
-    const name = document.getElementById("name").value;
-    const email = document.getElementById("email").value;
-    const message = document.getElementById("message").value;
-    
-    if (name && email && message) {
-        alert("Message sent! We'll get back to you soon.");
-    } else {
-        alert("Please fill in all the fields.");
-    }
+
+document.getElementById('createInvoice').addEventListener('click', function() {
+    alert('Invoice creation will be available soon!');
 });
 
-function createInvoice() {
-    alert("Invoice Tool coming soon!");
-}
-
-function createContract() {
-    alert("Contract Tool coming soon!");
-}
-
-function createWaiver() {
-    alert("Waiver Tool coming soon!");
-}
+document.getElementById('donateButton').addEventListener('click', function() {
+    alert('Thank you for supporting the Jumper Man App!');
+});
